69859172a0125d0bd363a249ec43bcf16b84530e
Natalia Yarmolinskaya <nat_yarm@natalias-mbp.kpn>
2025-01-16 21:37:19 +0100

    create pagination component

--- src/features/bookings/BookingTable.jsx
+++ src/features/bookings/BookingTable.jsx
@@ -4,6 +4,7 @@ import Menus from '../../ui/Menus';
 import Empty from '../../ui/Empty';
 import { useBookings } from './useBookings';
 import Spinner from '../../ui/Spinner';
+import Pagination from '../../ui/Pagination';
 
 function BookingTable() {
   const { bookings = [], isPending } = useBookings();
@@ -29,6 +30,10 @@ function BookingTable() {
             <BookingRow booking={booking} key={booking.id} />
           ))}
         </Table.Body>
+
+        <Table.Footer>
+          <Pagination count={45} />
+        </Table.Footer>
       </Table>
     </Menus>
   );
