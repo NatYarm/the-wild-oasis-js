892dc53659a90e1c61ab400451a39e98c66162b7
Natalia Yarmolinskaya <nat_yarm@natalias-mbp.kpn>
2025-02-01 22:09:50 +0100

    build dashboard page: hotel statistics and area chart

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,10 @@
-import styled from "styled-components";
+import styled from 'styled-components';
+import Statistics from './Statistics';
+import Spinner from '../../ui/Spinner';
+import { useRecentBookings } from './useRecentBookings';
+import { useRecentStays } from './useRecentStays';
+import { useCabins } from '../cabins/useCabins';
+import SalesChart from './SalesChart';
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -6,3 +12,26 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+  const { bookings, isPendingBookings } = useRecentBookings();
+  const { stays, confirmedStays, isPendingStays, numDays } = useRecentStays();
+  const { cabins, isPendingCabins } = useCabins();
+
+  if (isPendingBookings || isPendingStays || isPendingCabins)
+    return <Spinner />;
+  return (
+    <StyledDashboardLayout>
+      <Statistics
+        bookings={bookings}
+        confirmedStays={confirmedStays}
+        numDays={numDays}
+        cabinCount={cabins?.length}
+      />
+      <div>Todays activity </div>
+      <SalesChart bookings={bookings} numDays={numDays} />
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
