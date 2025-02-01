0b16a2cf83b4f13fb02d03da3387419e7a25caac
Natalia Yarmolinskaya <nat_yarm@natalias-mbp.kpn>
2025-02-02 00:02:02 +0100

    fixing bugs

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import FormRowVertical from '../../ui/FormRowVertical';
 import { useLogin } from './useLogin';
 
 function LoginForm() {
-  const [email, setEmail] = useState('leta@test.com');
-  const [password, setPassword] = useState('Pass12!');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const { login, isPending } = useLogin();
 
   function handleSubmit(e) {
